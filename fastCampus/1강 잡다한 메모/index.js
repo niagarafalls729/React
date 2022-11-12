d0d8f03124639557e4c2411460bf031a3576219b
niagarafalls729 <wltn729@gmail.com>
2022-11-12 11:09:49 +0900

    강의 메모 끄적

---
+++ fastCampus/1강 잡다한 메모/index.js
@@ -0,0 +1,7 @@
+//node index.js
+//npm instll colors
+// console.log("HI".rainbow);
+// console.log(('Double Raindows All Day Long').rainbow);
+
+console.log("HI");
+console.log(('Double Raindows All Day Long'));
